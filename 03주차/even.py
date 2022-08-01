039baec986f925d36159d3483bdfbc0759616363
dana <jisu2433@gmail.com>
2022-08-01 21:46:51 +0900

    Add : 짝수일 때 짝수와 중앙값 출력

--- 03주차/even.py
+++ 03주차/even.py
@@ -1,6 +1,12 @@
+import statistics
 n = int(input("첫 번째 수 입력 : "))
 m = int(input("두 번째 수 입력 : "))
 
-for i in range(n, m) :
-    if i%2==0 :
-        print(f'{i} 짝수')
+def find_even_number(n,m):
+    list = [n, m]
+    for i in range(n, m) :
+        if i%2==0 :
+            print(f'{i} 짝수')
+    if statistics.median(list)%2 == 0:
+        print(f'{int(statistics.median(list))} 중앙값')    
+find_even_number(n,m)
