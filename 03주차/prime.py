60c5bce1ff6fcf335f8d674757a61ab6ed3e83c7
dana <jisu2433@gmail.com>
2022-08-02 22:33:52 +0900

    add : 소수개수 구하기

--- 03주차/prime.py
+++ 03주차/prime.py
@@ -1,11 +1,18 @@
 n = int(input("첫 번째 수 입력 : "))
 m = int(input("두 번째 수 입력 : "))
 
-for i in range(n,m+1):
-    count = 0
-    for k in range(1, i+1):
-        if i%k == 0:
-            count += 1
-    if count <= 2:
-        print(f'소수 {i}')
+def count_prime_number(n, m):
+    cnt = 0 # 소수 개수
+    for i in range(n, m+1):
+        if i <= 1: # 0과 1은 소수가 아님
+            continue
+        
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            cnt += 1
+    return f"소수개수 {cnt}"
+
+print(count_prime_number(n, m))
 
